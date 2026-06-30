ffe9ed52d7c0b1a4901a90e3e4fa753fdaac72e8
SkillMasterShivam <shivamchaudhary5987@gmail.com>
2026-06-30 09:28:09 +0530

    delete unwanted files

--- backend/dist/middlewares/notFound.middleware.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.notFoundHandler = void 0;
-const errors_1 = require("../utils/errors");
-const notFoundHandler = (req, res, next) => {
-    next(new errors_1.NotFoundError(`Cannot find ${req.originalUrl} on this server`));
-};
-exports.notFoundHandler = notFoundHandler;
